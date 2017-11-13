a28bd9b6e4393a62bd240aed64a176296dfeb204
Arnold Koh <kohded@users.noreply.github.com>
2017-11-12 19:12:43 -0800

    Add configuration for production build on Heroku

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const path = require('path');
 const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 require('./models/users');
@@ -22,6 +23,14 @@ app.use(passport.session());
 
 require('./routes/auth-routes')(app); // Pass app to exported function when required.
 require('./routes/billing-routes')(app);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT);
